685e4f2568ce7e2e7fd4f7f8ffeede2c2bad5366
Sonny <sonnyasdev@gmail.com>
2024-12-21 04:01:33 +0100

    chore: add vite plugin to detect dead files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import deadFile from 'vite-plugin-deadfile';
 import eslint from 'vite-plugin-eslint';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), eslint(), tsconfigPaths()],
+  plugins: [
+    react(),
+    eslint(),
+    tsconfigPaths(),
+    deadFile({
+      root: 'src',
+    }),
+  ],
 });
