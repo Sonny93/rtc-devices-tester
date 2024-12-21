1c96e83d8da0b083242e017f7fe83afe066a2ea9
Sonny <sonnyasdev@gmail.com>
2024-12-21 04:36:11 +0100

    feat: change default color scheme

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <MantineProvider>
+  <MantineProvider defaultColorScheme="dark">
     <DevicesContextProvider>
       <SettingsContextProvider>
         <StreamContextProvider>
